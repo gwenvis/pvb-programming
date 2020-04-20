6158cf023855a36369d217f21fdff4220aec794f
MeesMD <meesmilan@hotmail.com>
2020-04-20 15:13:22 +0200

    Created a drag and drop system
    You can put this script on any object that you want to be drag and dropable

---
+++ Assets/Scripts/DragAndDropable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDropable : MonoBehaviour
+{
+
+    private bool isDragging;
+
+    public void OnMouseDown()
+    {
+        isDragging = true;
+    }
+
+    public void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    void Update()
+    {
+        if (isDragging)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePos);
+        }
+    }
+}
