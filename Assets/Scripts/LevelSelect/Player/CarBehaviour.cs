dd7abd63276bea9ab5f8059e14d847aa068e996c
Floyd Lehman <flehman2000@gmail.com>
2020-05-06 12:14:08 +0200

    Added prefab model of car and made player scripts plus scene

---
+++ Assets/Scripts/LevelSelect/Player/CarBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+namespace DN
+{
+	/// <summary>
+	/// ADD CLASS SUMMARY!
+	/// </summary>
+	public class CarBehaviour : MonoBehaviour
+	{
+		public Transform spherePos;
+
+		private Transform transform;
+
+		private void Start()
+		{
+			transform = GetComponent<Transform>();
+		}
+
+		private void Update()
+		{
+			//transform.rotation.x = new Vector3(0, transform.rotation.y, transform.rotation.z);
+			transform = spherePos;
+		}
+	}
+}
---
+++ Assets/Scripts/LevelSelect/Player/SphereMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace DN
+{
+	/// <summary>
+	/// ADD CLASS SUMMARY!
+	/// </summary>
+	public class SphereMovement : MonoBehaviour
+	{
+		[SerializeField] private float speed;
+
+		private Rigidbody rb;
+
+		private void Start()
+		{
+			rb = GetComponent<Rigidbody>();
+		}
+
+		private void Update()
+		{
+			float horizontalMovement = Input.GetAxis("Horizontal");
+			float verticalMovement = Input.GetAxis("Vertical");
+
+			Vector3 movementForce = new Vector3(horizontalMovement, 0.0f, verticalMovement);
+
+			rb.AddForce(movementForce * speed);
+		}
+	}
+}
