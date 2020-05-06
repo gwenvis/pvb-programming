786c72c1c9d46a53c63e528e39a9c0545fbe27a7
Floyd Lehman <flehman2000@gmail.com>
2020-05-06 13:51:28 +0200

    Made the car move with physics and implemented you can hit objects if they have physics script on them and made camera movement with the car

---
+++ Assets/Scripts/LevelSelect/Camera/VehicleCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class VehicleCamera : MonoBehaviour{
+	
+	[SerializeField] private Transform cameraRig;
+	
+	[SerializeField][Range(1, 20)] private float followSpeed	= 16;
+
+	private Vector3 cameraPositionOffset;
+	
+	private Camera camera;
+	
+	void Awake(){
+		
+		// Remember offset set in editor
+		
+		cameraPositionOffset = cameraRig.localPosition;
+		
+		// Get camera
+		
+		camera = cameraRig.GetChild(0).GetComponent<Camera>();
+	}
+	
+	void FixedUpdate(){
+		
+		// Camera follow
+		
+		cameraRig.position = Vector3.Lerp(cameraRig.position, transform.position + cameraPositionOffset, Time.deltaTime * followSpeed);
+		
+	}
+	
+}
