594f375431eb20f9c8d4db65a29f956e2f4200e1
Joppe van Basten <joppevanbasten@gmail.com>
2020-05-07 14:55:39 +0200

    DraggableBlocks for maze puzzle, also has IDroppable To connect them

---
+++ Assets/Scripts/Puzzle/MazePuzzle/MazeDraggableItem.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace DN.UI
+{
+	/// <summary>
+	/// ADD CLASS SUMMARY!
+	/// </summary>
+	public class MazeDraggableItem : DraggableItem, IEndDragHandler
+	{
+		public GameObject parentObject;
+		private float yOffset;
+		private float height;
+
+		private void Start()
+		{
+			height = GetComponent<RectTransform>().rect.height;
+		}
+	
+		public override void OnEndDrag(PointerEventData eventData)
+		{
+			canvasGroup.alpha = 1f;
+			canvasGroup.blocksRaycasts = true;
+
+			IDroppable dropZone = null;
+
+			foreach (RaycastHit2D hit in GetBoxCastHits())
+			{
+				if (hit.transform.GetComponent<IDroppable>() != null 
+				&& hit.transform != transform)
+				{
+					if (hit.transform.GetComponent<BlockDropZone>() == null)
+					{
+						dropZone = hit.transform.GetComponent<IDroppable>();
+					}
+					else if (hit.transform.GetComponent<BlockDropZone>().currentObj == null)
+					{
+						hit.transform.GetComponent<IDroppable>().Drop(this);
+					}
+				}
+			}
+			if (dropZone != null)
+			{
+				dropZone.Drop(this);
+				return;
+			}
+			DestroyAllChildren();
+		}
+
+		public void DestroyAllChildren()
+		{
+			(GetComponent<BlockDropZone>().currentObj as MazeDraggableItem)?.DestroyAllChildren();
+			Destroy(gameObject);
+		}
+
+		public void SetParent(GameObject parent, float offset)
+		{
+			parentObject = parent;
+			yOffset = offset;
+		}
+
+		private void Update()
+		{
+			if (parentObject != null)
+			{
+				transform.position = new Vector2(parentObject.transform.position.x, parentObject.transform.position.y - height - yOffset);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Puzzle/MazePuzzle/SpawnBlock.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace DN.UI
+{
+	/// <summary>
+	/// Spawns Block when clicked.
+	/// </summary>
+	public class SpawnBlock : MonoBehaviour, IDragHandler
+	{
+		[SerializeField]private GameObject draggableObject;
+		private DraggableItem draggableItem;
+		private Canvas canvas;
+
+		public void SetDraggableItem(GameObject draggableItem)
+		{
+			draggableObject = draggableItem;
+		}
+
+		public void SetCanvas(Canvas newCanvas)
+		{
+			canvas = newCanvas;
+		}
+
+		public void OnDrag(PointerEventData eventData)
+		{
+			GameObject block = Instantiate(draggableObject, transform.position, transform.rotation, canvas.transform);
+			draggableItem = block.GetComponent<DraggableItem>();
+			draggableItem.SetCanvas(canvas);
+			eventData.pointerDrag = block;
+		}
+	}
+}
