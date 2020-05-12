6d1ef47229441673895641e289b817785cd7cdfb
Floyd Lehman <flehman2000@gmail.com>
2020-05-12 12:31:18 +0200

    Edited biome controller and made a border control

--- Assets/Scripts/LevelSelect/Levels/BiomeController.cs
+++ Assets/Scripts/LevelSelect/Levels/BiomeController.cs
@@ -3,97 +3,146 @@ using UnityEngine;
 
 namespace DN.LevelSelect
 {
-	/// <summary>
-	/// This class look if the biome is cleared and gets the current biome and levels of the biome.
-	/// </summary>
-	public class BiomeController : MonoBehaviour
-	{
-		[SerializeField] private GameObject[] biomes;
-		[SerializeField] private GameObject[] levels;
+    /// <summary>
+    /// This class look if the biome is cleared and gets the current biome and levels of the biome.
+    /// </summary>
+    public class BiomeController : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] biomes;
+        [SerializeField] private GameObject[] levels;
 
-		private GameObject currentBiomeObj;
+        [SerializeField] private Transform borderParent;
+        private GameObject[] borders;
 
-		private int currentBiome;
-		private int biomeCount;
+        [SerializeField] private BiomeUI biomeUI;
 
-		private int currentLevelsCompleted ;
-		private int minBiomeLevelsCompleted = 0;
-		private int maxBiomeLevelsCompleted;
+        private GameObject currentBiomeObj;
+        private GameObject previousBiome;
 
-		private void Awake()
-		{
-			SetBiomeStart();
-			GetBiomeLevels();
-		}
+        private int currentBiome;
+        private int biomeCount;
 
-		private void Update()
-		{
-			if (Input.GetKeyDown(KeyCode.Backspace))
-			{
-				CheckForCompletionBiome();
-			}
-		}
+        private int currentLevelsCompleted;
+        private int minBiomeLevelsCompleted = 0;
+        private int maxBiomeLevelsCompleted;
 
-		private void SetBiomeStart()
-		{
-			currentBiome = 0;
-			biomes = new GameObject[transform.childCount];
-			for (int i = 0; i < transform.childCount; i++)
-			{
-				biomes[i] = transform.GetChild(i).gameObject;
-				biomeCount++;
-			}
-			currentBiomeObj = biomes[currentBiome];
-		}
+        private bool isBiomeFinished;
+        private bool isGameFinished;
 
-		private void GetBiomeLevels()
-		{
-			levels = new GameObject[currentBiomeObj.transform.childCount];
-			for (int i = 0; i < currentBiomeObj.transform.childCount; i++)
-			{
-				levels[i] = currentBiomeObj.transform.GetChild(i).gameObject;
-			}
-		}
+        private void Awake()
+        {
+            SetBiomeStart();
+            SetBordersStart();
+            GetBiomeLevels();
+        }
 
-		private void CheckForCompletionBiome()
-		{
-			maxBiomeLevelsCompleted = levels.Length;
-			for (int i = 0; i < levels.Length; i++)
-			{
-				if (levels[i].GetComponent<LevelData>().isCompleted)
-				{
-					currentLevelsCompleted++;
-				}
-				else
-				{
-					currentLevelsCompleted = minBiomeLevelsCompleted;
-					break;
-				}
-			}
-			if(currentLevelsCompleted == maxBiomeLevelsCompleted)
-			{
-				currentLevelsCompleted = minBiomeLevelsCompleted;
-				currentBiome++;
-				currentBiomeObj = biomes[currentBiome];
-				ClearLevels();
-				GetBiomeLevels();
-			}
-		}
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                CheckForCompletionBiome();
+            }
+        }
 
-		private void ClearLevels()
-		{
-			for (int i = 0; i < levels.Length; i++)
-			{
-				levels[i] = null;
-			}
-		}
+        private void CheckForCompletionBiome()
+        {
+            maxBiomeLevelsCompleted = levels.Length;
 
-		private void CheckForAllBiomesCleared()
-		{
-			if(currentBiome >= biomeCount)
-			{
-				Debug.Log("Finished Game biem");
-			}
-		}
-	}
+            for (int i = 0; i < levels.Length; i++)
+            {
+                if (levels[i].GetComponent<LevelData>().isCompleted)
+                {
+                    currentLevelsCompleted++;
+                    levels[i].SetActive(false);
+                }
+            }
+
+            if (currentLevelsCompleted == maxBiomeLevelsCompleted)
+            {
+                previousBiome = currentBiomeObj;
+                currentLevelsCompleted = minBiomeLevelsCompleted;
+                currentBiome++;
+
+                if(currentBiome != biomeCount)
+                {
+                    currentBiomeObj = biomes[currentBiome];
+                }
+
+                isBiomeFinished = true;
+                NextBiome(isBiomeFinished, currentBiome);
+            }
+            else
+            {
+                biomeUI.SetTaskManager(isBiomeFinished, (maxBiomeLevelsCompleted - currentLevelsCompleted));
+                currentLevelsCompleted = minBiomeLevelsCompleted;
+                isBiomeFinished = false;
+            }
+        }
+        private void SetBordersStart()
+        {
+            borders = new GameObject[borderParent.childCount];
+            for (int i = 0; i < borderParent.childCount; i++)
+            {
+                borders[i] = borderParent.GetChild(i).gameObject;
+            }
+        }
+
+        private void GetBiomeLevels()
+        {
+            levels = new GameObject[currentBiomeObj.transform.childCount];
+            for (int i = 0; i < currentBiomeObj.transform.childCount; i++)
+            {
+                levels[i] = currentBiomeObj.transform.GetChild(i).gameObject;
+            }
+        }
+
+        private void SetBiomeStart()
+        {
+            currentBiome = 0;
+            biomes = new GameObject[transform.childCount];
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                biomes[i] = transform.GetChild(i).gameObject;
+                biomes[i].SetActive(false);
+                biomeCount++;
+            }
+            currentBiomeObj = biomes[currentBiome];
+            currentBiomeObj.SetActive(true);
+        }
+
+        private void NextBiome(bool value, int currentCompletedBiome)
+        {
+            ClearLevels();
+            GetBiomeLevels();
+            CheckForAllBiomesCleared();
+            biomeUI.SetTaskManager(isBiomeFinished, (maxBiomeLevelsCompleted - currentLevelsCompleted));
+
+            currentBiomeObj.SetActive(value);
+            previousBiome.SetActive(!value);
+
+            UpdateBorders(currentCompletedBiome);
+        }
+
+        private void UpdateBorders(int currentBiomeCleared)
+        {
+            borders[currentBiomeCleared - 1].SetActive(false);
+        }
+
+        private void ClearLevels()
+        {
+            for (int i = 0; i < levels.Length; i++)
+            {
+                levels[i] = null;
+            }
+        }
+
+        private void CheckForAllBiomesCleared()
+        {
+            if (currentBiome >= biomeCount)
+            {
+                isGameFinished = true;
+                Debug.Log("Finished Game biem");
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/LevelSelect/Levels/BiomeUI.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DN
+{
+	/// <summary>
+	/// ADD CLASS SUMMARY!
+	/// </summary>
+	public class BiomeUI : MonoBehaviour
+	{
+		[SerializeField] private Text taskText;
+		[SerializeField] private Text finishedBiomeText;
+
+		private const string TASK_MANAGER_BEGIN = "Help nog ";
+		private const string TASK_MANAGER_EIND = " robots in dit gebied!";
+		private const string TASK_MANAGER_EIND_SINGULAR = " robot in dit gebied!";
+
+		public void SetTaskManager(bool biomeIsFinished, int robotsNeedFixing)
+		{
+			if (!biomeIsFinished)
+			{
+				taskText.gameObject.SetActive(!biomeIsFinished);
+				finishedBiomeText.gameObject.SetActive(biomeIsFinished);
+				if(Mathf.Approximately(robotsNeedFixing, 1))
+				{
+					taskText.text = TASK_MANAGER_BEGIN + robotsNeedFixing + TASK_MANAGER_EIND_SINGULAR;
+				}
+				else
+				{
+					taskText.text = TASK_MANAGER_BEGIN + robotsNeedFixing + TASK_MANAGER_EIND;
+				}
+			}
+			else
+			{
+				taskText.gameObject.SetActive(!biomeIsFinished);
+				finishedBiomeText.gameObject.SetActive(biomeIsFinished);
+			}
+		}
+	}
+}
