7f6a6d91a8df5922d264bcc4b682d9478efc900d
Antonio Bottelier <Antonio3338@gmail.com>
2020-05-21 15:04:58 +0200

    Fixed level continue and restart screens

--- Assets/Scripts/UI/Screens/WinLoseScreenController.cs
+++ Assets/Scripts/UI/Screens/WinLoseScreenController.cs
@@ -6,112 +6,124 @@ using DN.Tutorial;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using LevelLoader = DN.LevelSelect.SceneManagment.LevelLoader;
 
 namespace DN.UI
 {
-	/// <summary>
-	/// Controlls the win and lose screen
-	/// </summary>
-	public class WinLoseScreenController : MonoBehaviour
-	{
-		[SerializeField] private Lives lives;
-		[SerializeField] private GameObject loseScreen;
-		[SerializeField] private GameObject winScreen;
-		[SerializeField] private WinController winController;
-		[SerializeField] private Puzzle.Color.LevelLoader colorPuzzleLevelLoader;
+    /// <summary>
+    /// Controlls the win and lose screen
+    /// </summary>
+    public class WinLoseScreenController : MonoBehaviour
+    {
+        [SerializeField] private Lives lives;
+        [SerializeField] private GameObject loseScreen;
+        [SerializeField] private GameObject winScreen;
+        [SerializeField] private WinController winController;
+        [SerializeField] private Puzzle.Color.LevelLoader colorPuzzleLevelLoader;
 
-		[SerializeField] private UIAssistant assistant;
+        [SerializeField] private UIAssistant assistant;
 
-		private LevelLoader levelLoader;
+        [Header("Buttons")] [SerializeField] private Button winContinueButton;
+        [SerializeField] private Button loseContinueButton;
+        [SerializeField] private Button loseRestartButton;
 
-		private string puzzleName;
+        private LevelLoader levelLoader;
 
-		private void Start()
-		{
-			levelLoader = ServiceLocator.Locate<LevelMemoryService>().LevelLoader;
+        private string puzzleName;
 
-			loseScreen.SetActive(false);
-			winScreen.SetActive(false);
-			lives.AllLifeLost += OnAllLifeLost;
-			winController.GameWonEvent += OnGameWonEvent;
-		}
+        private void Start()
+        {
+            levelLoader = ServiceLocator.Locate<LevelMemoryService>().LevelLoader;
 
-		private void OnEnable()
-		{
-			Player.RunFinishedEvent += OnRunFinishedEvent;
-		}
+            loseScreen.SetActive(false);
+            winScreen.SetActive(false);
+            lives.AllLifeLost += OnAllLifeLost;
+            winController.GameWonEvent += OnGameWonEvent;
+        }
 
-		private void OnDisable()
-		{
-			Player.RunFinishedEvent -= OnRunFinishedEvent;
-		}
+        private void OnEnable()
+        {
+            Player.RunFinishedEvent += OnRunFinishedEvent;
+            winContinueButton.onClick.AddListener(ContinueToLevelselectWon);
+            loseContinueButton.onClick.AddListener(ContinueToLevelselectLose);
+            loseRestartButton.onClick.AddListener(ReloadScene);
+        }
 
-		private void OnRunFinishedEvent(Player.State obj)
-		{
-			if (obj == Player.State.Done)
-			{
-				OnGameWonEvent();
-				return;
-			}
-			Stuck();
-		}
+        private void OnDisable()
+        {
+            Player.RunFinishedEvent -= OnRunFinishedEvent;
+            winContinueButton.onClick.RemoveListener(ContinueToLevelselectWon);
+            loseContinueButton.onClick.RemoveListener(ContinueToLevelselectLose);
+            loseRestartButton.onClick.RemoveListener(ReloadScene);
+        }
 
-		private void OnGameWonEvent()
-		{
-			winScreen.SetActive(true);
-		}
+        private void OnRunFinishedEvent(Player.State obj)
+        {
+            if (obj == Player.State.Done)
+            {
+                OnGameWonEvent();
+                return;
+            }
 
-		private void Stuck()
-		{
-			if (colorPuzzleLevelLoader) colorPuzzleLevelLoader.Clean();
+            Stuck();
+        }
 
-			lives.LoseLife();
-			var livesService = ServiceLocator.Locate<LivesService>();
-			livesService.SetCurrentLives(lives.CurrentLives, true);
+        private void OnGameWonEvent()
+        {
+            winScreen.SetActive(true);
+        }
 
-			if (livesService.CurrentLives <= 0) return;
-			
-			if(colorPuzzleLevelLoader) colorPuzzleLevelLoader.LoadWithExistingData();
-		}
+        private void Stuck()
+        {
+            if (colorPuzzleLevelLoader) colorPuzzleLevelLoader.Clean();
 
-		private IEnumerator Reload(Scene prevScene, string c)
-		{
-			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Additive);
-			yield return new WaitForSeconds(.1f);
-			SceneManager.UnloadScene(prevScene);
-			SceneManager.SetActiveScene(SceneManager.GetSceneByName(c));
-		}
+            lives.LoseLife();
+            var livesService = ServiceLocator.Locate<LivesService>();
+            livesService.SetCurrentLives(lives.CurrentLives, true);
 
-		private void OnAllLifeLost()
-		{
-			ResetLives();
-			loseScreen.SetActive(true);
-		}
+            if (livesService.CurrentLives <= 0) return;
 
-		public void ReloadScene()
-		{
-			ResetLives();
-			Scene prevScene = SceneManager.GetActiveScene();
-			puzzleName = SceneManager.GetActiveScene().name;
-			StartCoroutine(Reload(prevScene, puzzleName));
-		}
+            if (colorPuzzleLevelLoader) colorPuzzleLevelLoader.LoadWithExistingData();
+        }
 
-		public void ContinueToLevelselectLose()
-		{
-			ResetLives();
-			levelLoader.LoadLevelSelectFromPuzzle(false);
-		}
+        private IEnumerator Reload(Scene prevScene, string c)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Additive);
+            yield return new WaitForSeconds(.1f);
+            SceneManager.UnloadScene(prevScene);
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName(c));
+        }
 
-		public void ContinueToLevelselectWon()
-		{
-			ResetLives();
-			levelLoader.LoadLevelSelectFromPuzzle(true);
-		}
+        private void OnAllLifeLost()
+        {
+            ResetLives();
+            loseScreen.SetActive(true);
+        }
 
-		private void ResetLives()
-		{
-			ServiceLocator.Locate<LivesService>().SetCurrentLives(3, false);
-		}
-	}
+        public void ReloadScene()
+        {
+            ResetLives();
+            Scene prevScene = SceneManager.GetActiveScene();
+            puzzleName = SceneManager.GetActiveScene().name;
+            StartCoroutine(Reload(prevScene, puzzleName));
+        }
+
+        public void ContinueToLevelselectLose()
+        {
+            ResetLives();
+            levelLoader.LoadLevelSelectFromPuzzle(false);
+        }
+
+        public void ContinueToLevelselectWon()
+        {
+            ResetLives();
+            levelLoader.LoadLevelSelectFromPuzzle(true);
+        }
+
+        private void ResetLives()
+        {
+            ServiceLocator.Locate<LivesService>().SetCurrentLives(3, false);
+        }
+    }
 }
