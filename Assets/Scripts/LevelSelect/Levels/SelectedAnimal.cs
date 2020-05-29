c25f03d409e315f461a43c8384a2cb23c616953d
Floyd Lehman <flehman2000@gmail.com>
2020-05-29 15:43:38 +0200

    Fixed tutorial and animal images

--- Assets/Scripts/LevelSelect/Levels/SelectedAnimal.cs
+++ Assets/Scripts/LevelSelect/Levels/SelectedAnimal.cs
@@ -2,7 +2,7 @@
 {
     public partial class LevelDataEditor
     {
-        public enum SelectedAnimal { None, Bug, Hog, Shark, Penguin, Racoon, Sealion, Walrus, Cricket, Sticktail, Dragonfly};
+        public enum SelectedAnimal { None, Bug, Hog, Shark, Penguin, Racoon, SeaLion, Walrus, Cricket, StickTail, DragonFly};
         public enum SelectedPuzzle { None, ColorPuzzle, MazePuzzle };
 
     }
